439032c5f6b4d3c50d857860967c8dc1bacf36a7
Sandesh <63111343+sandesh1337@users.noreply.github.com>
2024-02-01 12:27:36 +0000

    first commit
    commit

--- src/lib/parse.js
+++ src/lib/parse.js
@@ -2,6 +2,12 @@ export function parseTeamsJson(data) {
   return JSON.parse(data);
 }
 
+
+
+export function parseGameJson(data) {
+  return JSON.parse(data);
+}
+
 /**
  * Tekur `gameday` gögn, staðfestir og hendir ólöglegum
  * færslum, skilar á normalizeseruðu formi.
